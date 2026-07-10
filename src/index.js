05c09c9031a789af1a09645199123dfc16b62634
Nikhil Bobde <bobdenikhil53@gmail.com>
2026-07-10 16:36:12 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 
-import mongoose from "mongoose";
-import {DB_NAME} from "./constants.js"
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants.js"
 
 import connectDB from "./db/index.js";
-
-
+// import app from "app.js"
 dotenv.config({
-    path: './env' 
+    path: './env'
 })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`sever is running ar Port: ${process.env.PORT}`);
+
+        })
+
+        app.on("error", (error) => {
+            console.log("Error: ", error);
+            throw error
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO_DB Connection Failed!", err);
+
+    })
 
 
 /*
-//  approach 1   
+//  approach 1
 import express from "express"
 const app = express()
 
@@ -29,18 +43,18 @@ const app = express()
 
         app.listen(process.env.PORT, () => {
             console.log(`App is listening on port ${process.env.PORT}`);
-            
+
         })
     }
     catch(error) {
         console.log("error :" , error);
         throw error
-        
+
     }
 }) ()*/
 
 
 // function connectDB() {
-// 
+//
 // }
 // connectDB()
